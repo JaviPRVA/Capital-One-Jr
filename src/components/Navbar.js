164c785d0e51ab1981f84b856b40b395caa71119
Javier Ralat <javieraralat@vt.edu>
2024-05-16 13:46:02 -0400

    Updated NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import COjr from '../assets/COjr.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className=" bg-navbar-gradien p-4 text-white">
-            <div className="justify-center flex items-center">
+        <nav className="bg-navbar-gradient p-4 text-white flex items-center">
+            <div className="flex items-center">
                 <img src={COjr} alt="logo" className="mr-10 pl-5 w-[200px] h-auto" />
-                <h1 className="text-black ml-10 text-[30px]">Digital Piggy Bank</h1>
+            </div>
+            <div className="flex justify-end flex-1">
+             <Link to="/" className="text-[#D22C27] font-bold ml-10 text-[30px] pr-8">Home</Link>
+             <Link to="/" className="text-[#D22C27] font-bold ml-10 text-[30px] pr-8">Credit Cards</Link>
+             <Link to="/Quiz" className="text-[#D22C27] font-bold ml-10 text-[30px] pr-8">Test Your Knowledge</Link>
+             <Link to="/" className="text-[#D22C27] font-bold ml-10 text-[30px] pr-8">Our Mission</Link>
             </div>
         </nav>
+
     );
 };
 
