3dfdc72ad410cbfc6cfe2e4d3ee2624ef0052970
Javier Ralat <javieraralat@vt.edu>
2024-05-16 02:05:26 -0400

    possible first design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import piggy from './assets/piggy.png';
+
 
 function App() {
   return (
@@ -8,7 +10,10 @@ function App() {
          <Navbar />
        </div>
        <div className="bg-[#053C59] h-screen">
-           
+        <div className="justify-center mx-auto flex">
+        <img src={piggy} alt="piggy" className="pt-20 w-[400px] h-auto" />
+        </div>
+
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import COjr from '../assets/COjr.png';
 
 const Navbar = () => {
     return (
-        <nav className="bg-navbar-gradien p-4 text-white">
-            <div>
-                <img src={COjr} alt="logo" className="pl-5 w-[200px] h-auto" />
-
+        <nav className=" bg-navbar-gradien p-4 text-white">
+            <div className="justify-center flex items-center">
+                <img src={COjr} alt="logo" className="mr-10 pl-5 w-[200px] h-auto" />
+                <h1 className="text-black ml-10 text-[30px]">Digital Piggy Bank</h1>
             </div>
         </nav>
     );
